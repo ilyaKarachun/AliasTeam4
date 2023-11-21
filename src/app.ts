29b790c72b1952df6781192c37d28c1b14ad98fe
Matsvei Khmialnitski <geniusx1990@gmail.com>
2023-11-21 23:01:46 +0300

    feat: implemented working database and nodejs container via docker

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,37 @@
-import express from 'express';
-import { config } from 'dotenv';
+/* eslint-disable */
+import express, { Request, Response } from 'express';
+import dotenv from 'dotenv';
 import api from './router';
+import cors from 'cors';
 import errorHandlerMiddleWare from './middlewares/errorHandler.middleware';
+import { createDatabase, dbName } from './database/database';
 
-config();
+dotenv.config();
 const app = express();
+const port = process.env.PORT || 3000;
 
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use('/api/v1', api);
 app.use(errorHandlerMiddleWare);
 
-app.listen(process.env.PORT || 3000, () =>
-  console.log(`Running on port ${process.env.PORT}`),
+app.get('/', (request: Request, response: Response) => {
+  return response.json({
+    status: 'express server is running',
+  });
+});
+
+app.listen(port, () =>
+  console.log(`Running on port ${port}`),
 );
+
+async function startApp() {
+  try {
+    await createDatabase(dbName);
+  } catch (error) {
+    console.error('Error:', error);
+  }
+}
+
+startApp();
--- src/router/index.ts
+++ src/router/index.ts
@@ -2,7 +2,9 @@ import { Router } from 'express';
 const router = Router();
 
 import usersRouter from './users.router';
+import documentRouter from './document.router';
 
 router.use('/users', usersRouter);
+router.use('/document', documentRouter);
 
 export default router;
