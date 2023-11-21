3686dc04629c5fe7c00bba52e613aa58138dba0f
avaitovichSolvd <alinavfrontend@gmail.com>
2023-11-21 16:36:18 +0100

    DBTypes

---
+++ src/types/DBSheme.ts
@@ -0,0 +1,33 @@
+export interface User {
+  username: string;
+  password: string;
+  statistic?: string[];
+  status?: 'active' | 'not active';
+}
+
+export interface Team {
+  participants: string[];
+  chatID?: string;
+  words: string[];
+  score: {
+    word: string;
+    status?: 'guessed' | 'not guessed';
+    guessed?: string | 'not';
+  }[];
+}
+
+export interface Game {
+  status?: 'creating' | 'playing' | 'finished';
+  team1: Team;
+  team2: Team;
+  won?: string;
+}
+
+export interface Chat {
+  messages: {
+    timestamp: string;
+    sender: string;
+    type: 'description' | 'message';
+    content: string;
+  }[];
+}
